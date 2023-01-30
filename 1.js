141b8cb75283611ef95aedadcd771f5ba22f6925
飓枫 <112387102+HurricanMaple@users.noreply.github.com>
2023-01-30 10:43:09 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,10 @@
 
+iziToast.show({
+
+                            title: 'Hey',
+
+                            message: 'What would you like to add?'
+
+                        });
+
+                    
