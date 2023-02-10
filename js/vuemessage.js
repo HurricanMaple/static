6b983840db64798959106d2e4e97d8e4bc98dfb2
飓枫 <112387102+HurricanMaple@users.noreply.github.com>
2023-02-10 23:11:35 +0800

    Update vuemessage.js

--- js/vuemessage.js
+++ js/vuemessage.js
@@ -5,7 +5,7 @@ document.addEventListener("copy",function(e){
             this.$notify({
                 title:"嘿！复制成功",
                 message:"若要转载请务必保留原文链接！爱你呦~",
-                position: 'bottom-right',
+                position: 'top-right',
                 offset: 50,
                 showClose: false,
                 type:"success"
@@ -14,3 +14,44 @@ document.addEventListener("copy",function(e){
         }
     })
 })
+/* 禁用F12按键并提醒 */
+
+document.onkeydown = function () {
+
+if (window.event && window.event.keyCode == 123) {
+
+  event.keyCode = 0;
+
+  event.returnValue = false;
+
+    new Vue({
+
+            data:function(){
+
+                this.$notify({
+
+                    title:"嘿！别瞎按",
+
+                    message:"坏孩子！",
+
+                    position: 'top-right',
+
+                    offset: 50,
+
+                    showClose: false,
+
+                    type:"error"
+
+                });
+
+                return{visible:false}
+
+            }
+
+        })
+
+  return false;
+
+}
+
+};
